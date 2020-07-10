c0ca941ba404bbb33f171d990b3e8ab003ba6dcc
Oziel <ozielalves@ufrn.edu.br>
2020-07-09 22:23:07 -0300

    UserNav sidebar done

--- src/pages/Sorteios-Abertos/index.tsx
+++ src/pages/Sorteios-Abertos/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Container, Col, Row, Nav } from 'react-bootstrap';
 
 import './styles.css';
 
+import Avatar from '../../assets/Avatar.png';
 import UserNav from '../User-Navigation';
 
 const SorteiosAbertos = () => {
   return(
     <div id="page-sorteios">
+      <UserNav />
       <div className="content">
-        <UserNav />
         <h1>Sorteios Abertos</h1>
       </div>
     </div>
--- src/pages/User-Navigation/index.tsx
+++ src/pages/User-Navigation/index.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Container, Col, Row, Nav } from 'react-bootstrap'
 
 import './styles.css';
+import Logo from '../../assets/littleLogo.png';
+import Avatar from '../../assets/Avatar.png';
 
-const UserNav = () => {
-  return (
-    <h1>ss</h1>
+
+const UserNav = () =>{
+  return(
+    <Nav id="sidebar">
+        <div className="sidebar-header">
+          <img src={Avatar} alt="Avatar"/>
+        </div>
+
+        <ul className="list-unstyled components">
+            <h3>Oziel Alves</h3>
+            <li className="active">
+                <ul className="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">127 Fichas</a>
+                    </li>
+                    <li>
+                        <a href="#">Editar Perfil</a>
+                    </li>
+                    <li>
+                        <a href="#">Sair</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+      </Nav>
   );
 }
 
