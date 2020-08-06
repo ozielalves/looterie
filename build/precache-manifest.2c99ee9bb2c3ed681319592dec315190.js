c7ad8cfe072e8791e965b30766f09334158c25f5
Oziel <ozielalves@ufrn.edu.br>
2020-08-06 20:56:34 -0300

    build folder

---
+++ build/precache-manifest.2c99ee9bb2c3ed681319592dec315190.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff721487f5bd1a9f064082008090dd1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda62e9a77a114fd0dae",
+    "url": "/static/css/main.4d99336c.chunk.css"
+  },
+  {
+    "revision": "23636ecf75416dad4894",
+    "url": "/static/js/2.e6a58e14.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e6a58e14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda62e9a77a114fd0dae",
+    "url": "/static/js/main.138e99b5.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "771af3b6f10801a37025ca079721879e",
+    "url": "/static/media/Avatar.771af3b6.png"
+  },
+  {
+    "revision": "57915142c7e277e258826cd9f20c59df",
+    "url": "/static/media/Bolinhas-home-mobile.57915142.png"
+  },
+  {
+    "revision": "772f1ed70cb6d064fcc3d9c441e03fab",
+    "url": "/static/media/Cadastro-bolinhas.772f1ed7.png"
+  },
+  {
+    "revision": "da4fb00cf57d5b256ca2f2a2545548b2",
+    "url": "/static/media/Home-bolinhas.da4fb00c.png"
+  },
+  {
+    "revision": "402638557c474523141b9a2a041d174f",
+    "url": "/static/media/Login-bolinhas.40263855.png"
+  },
+  {
+    "revision": "a1efc5056ef97c537b4debeffabeaf6a",
+    "url": "/static/media/Logo.a1efc505.png"
+  },
+  {
+    "revision": "2be5c97f37a64a752e7d14530fb6cd89",
+    "url": "/static/media/Plano-bolinhas.2be5c97f.png"
+  },
+  {
+    "revision": "96464ced6ac9f5903e91e042a35fd452",
+    "url": "/static/media/Sorteio1.96464ced.png"
+  },
+  {
+    "revision": "9a76d8500b6fdf91f8a988fe13f089de",
+    "url": "/static/media/Sorteio2.9a76d850.png"
+  },
+  {
+    "revision": "c9841f4020a26c800438839be929872d",
+    "url": "/static/media/Sorteio3.c9841f40.png"
+  },
+  {
+    "revision": "48e11b84ead743f60a094fc94e2c76ee",
+    "url": "/static/media/Sorteio4.48e11b84.png"
+  },
+  {
+    "revision": "bff9214a9631dc1be189befc5d0ce1e6",
+    "url": "/static/media/UserNav-bolinhas.bff9214a.png"
+  }
+]);
