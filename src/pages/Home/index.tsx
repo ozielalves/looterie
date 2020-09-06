ff45c3fea16bd0db050358e025b7763b443fb81a
Oziel <ozielalves@ufrn.edu.br>
2020-09-06 20:19:02 -0300

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { BsList } from "react-icons/bs";
 
@@ -13,25 +13,47 @@ const Home = () => {
 
   const [navDisplay, setNavDisplay] = useState(false);
 
+  const navComponent = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // add when mounted
+    document.addEventListener("mousedown", handleClick);
+    // return function to be called when unmounted
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
+  const handleClick = e => {
+    if (navComponent != null) {
+      if (navComponent.current.contains(e.target)) {
+        // inside click
+        return;
+      }
+      // outside click
+      setNavDisplay(!navDisplay)
+    } 
+  };
+
   const navDisplayHandler = () => {
     setNavDisplay(!navDisplay)
   }
 
   return(
     <div id="home">
-        <div className="home-header">
-          <Link to="/home"><img src={Logo} alt="logo"/></Link>
-          <p className="welcome"> Bem vindo(a), <strong>OZIEL</strong></p>
-          <div className="burguer" onClick={navDisplayHandler}>
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
-          </div>
-        </div>
-        <div id="nav" className={ navDisplay ? 'visible' : 'not-visible'}>
+        <div ref={navComponent} id="nav" className={ navDisplay ? 'visible' : 'not-visible'}>
           <UserNav />
         </div>
-        <main>
+        <main className={navDisplay ? 'darken' : ''}>
+          <div className="home-header">
+            <Link to="/home"><img src={Logo} alt="logo"/></Link>
+            <p className="welcome"> Bem vindo(a), <strong>OZIEL</strong></p>
+            <div className="burguer" onClick={navDisplayHandler}>
+              <div className="line1"></div>
+              <div className="line2"></div>
+              <div className="line3"></div>
+            </div>
+          </div>
           <div className="sorteios">
             {/* <Sorteios /> */}
           </div>
