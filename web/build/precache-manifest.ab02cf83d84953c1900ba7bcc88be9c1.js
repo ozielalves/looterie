166a17223d6e6c0290fa6292a043821d35d76e77
Oziel <ozielalves@ufrn.edu.br>
2020-10-26 22:40:34 -0300

    build

---
+++ web/build/precache-manifest.ab02cf83d84953c1900ba7bcc88be9c1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c410a4dd48e003309c645ce06caf3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed18fdef136cb48e3e4f",
+    "url": "/static/css/main.e50d744a.chunk.css"
+  },
+  {
+    "revision": "528e20fd9c8fd1e53652",
+    "url": "/static/js/2.ad90336e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ad90336e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed18fdef136cb48e3e4f",
+    "url": "/static/js/main.531da6fe.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "da4fb00cf57d5b256ca2f2a2545548b2",
+    "url": "/static/media/Home-bolinhas.da4fb00c.png"
+  },
+  {
+    "revision": "a1efc5056ef97c537b4debeffabeaf6a",
+    "url": "/static/media/Logo.a1efc505.png"
+  },
+  {
+    "revision": "a25b44618c89ee6f0006712c9c1a24dc",
+    "url": "/static/media/Mini-Logo.a25b4461.svg"
+  },
+  {
+    "revision": "697b77273d5f40e13166e3f5bdb4f052",
+    "url": "/static/media/Plano-bolinhas-cadastro.697b7727.png"
+  },
+  {
+    "revision": "bff9214a9631dc1be189befc5d0ce1e6",
+    "url": "/static/media/UserNav-bolinhas.bff9214a.png"
+  },
+  {
+    "revision": "3033e43e6671e34bc044cbc3b35e309c",
+    "url": "/static/media/user-avatar.3033e43e.svg"
+  }
+]);
